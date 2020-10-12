b31193462c7cf707abc946ef3ba166d57a4cb524
amysujunglee <amy.sujunglee@gmail.com>
2020-10-11 17:15:22 -0700

    creating startGame function

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+// Create a list of questions
 // Start the game - after clicking 'Start' button
 // Show the next question - after clicking 'Next' button
 // Select the answer - showing an effect when the user selects a correct answer or wrong answer
+
+const startBtn = document.querySelector('#start-btn');
+const questionContainer = document.querySelector('#question-container');
+
+startBtn.addEventListener('click', startGame);
+
+function startGame() {
+    console.log('start game!');
+    startBtn.classList.add('hide');
+    questionContainer.classList.remove('hide')
+    setNextQuestion();
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
