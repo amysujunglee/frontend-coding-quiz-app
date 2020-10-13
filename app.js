a933c5660264f36598b0cd38ab71536924446910
amysujunglee <amy.sujunglee@gmail.com>
2020-10-12 22:30:45 -0700

    adding answers

--- app.js
+++ app.js
@@ -27,6 +27,16 @@ function setNextQuestion() {
 
 function showQuestion(question) {
     questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+        answerBtns.appendChild(button);
+    });
 }
 
 function selectAnswer() {
